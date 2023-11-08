b480c9cf728955061e4066c9a42681a51a75ba92
ajay <ajay@ubuntu22.04.myguest.virtualbox.org>
2023-11-08 22:08:56 +0530

    Symfony6.3x Faker Factory Ex:

--- fakerfactories/src/Controller/PostController.php
+++ fakerfactories/src/Controller/PostController.php
@@ -6,16 +6,39 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/post', name: 'app_post')]
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Post;
+use Faker;
+
+
+
 class PostController extends AbstractController
 {
-    #[Route('/', name: 'app_post')]
+    #[Route('/', name: 'post_index')]
     public function index(): Response
     {
-        echo "Post Controller";
-        exit;  
+
         return $this->render('post/index.html.twig', [
             'controller_name' => 'PostController',
         ]);
+    }
+
+
+    #[Route('/faker', name: 'post_faker')]
+    public function faker(EntityManagerInterface $entityManager): Response
+    {
+        $faker = Faker\Factory::create();
+     //Insert 10 records
+      for($i=0;$i<10;$i++){
+        $post = new Post();
+        $post->setTitle($faker->name);
+        $post->setDescription($faker->country);
+
+        $entityManager->persist($post);
+        $entityManager->flush();
+       }
+        exit;
+
     }
 }
